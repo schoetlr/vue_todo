fcc046fe67c9d4290da324878bffb8134ef82536
Luke Schoettinger <srlrs20020@gmail.com>
2019-07-09 09:55:39 -0400

    todo component working

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    todos: [
+      { id: 0, text: 'Clean Kitchen' },
+      { id: 1, text: 'Mow grass' },
+      { id: 2, text: 'Read book' }
+    ]
   }
 });
 
-console.log("loaded");
+Vue.component('todo-item', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
